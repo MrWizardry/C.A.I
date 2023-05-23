4463177c86a209e7d16950108d6aa2ef11bb8d5b
MrWizardry <pedro.silva25@estudante.ifms.edu.br>
2023-05-22 21:19:33 -0300

    Movimentação

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -8,54 +8,64 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
     public Animator animator;
     
-
-    [SerializeField] private float speed = 6f;
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed = 6f;
     [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float sensivity = 1f;
+    [SerializeField] private float cameraSensitivity = 3f;
+
     private float turnSmoothVelocity;
 
-    
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        
-
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
-                turnSmoothTime);
-            
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * (speed * Time.deltaTime));
-            
+            RotatePlayer(direction);
+            MovePlayer(direction);
             animator.SetBool("IsWalking", true);
         }
         else
             animator.SetBool("IsWalking", false);
-        
-        
-        
-        //----------------- Mouse ------------------------//
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
 
         if (Input.GetKeyDown(KeyCode.G))
-        {
             Application.Quit();
-        }
     }
+
+    private void RotatePlayer(Vector3 direction)
+    {
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        float mouseX = Input.GetAxis("Mouse X") * cameraSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * cameraSensitivity;
+
+        cam.Rotate(Vector3.up, mouseX, Space.World);
+        cam.Rotate(Vector3.left, mouseY, Space.Self);
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        Vector3 moveDir = Quaternion.Euler(0f, cam.eulerAngles.y, 0f) * Vector3.forward;
+        controller.Move(moveDir.normalized * (movementSpeed * Time.deltaTime * sensivity));
+    }
+
+    
 }
